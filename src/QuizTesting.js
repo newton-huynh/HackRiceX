74bb3cbed74922e4e0bfb8289e1dd62516aa6a2b
Vivian Ellis <vce1@rice.edu>
2020-09-19 14:23:31 -0500

    Form testing.

--- src/QuizTesting.js
+++ src/QuizTesting.js
@@ -27,18 +27,71 @@ class SelectElement extends React.Component {
     }
 }
 
+function getLocations(info) {
+    let locations = [];
+    for (const record of info.records) {
+        const city = record.fields.CITY;
+        const state = record.fields.STATE;
+        // TODO: check if location already in locations
+        locations.push({
+            value: city + ", " + state,
+            label: city + ", " + state,
+        });
+    }
+    return locations;
+}
+
+const sheetInfo = {
+    "records": [
+        {
+            "id": "recdqU4dbZsh2CsYk",
+            "fields": {
+                "STATE": "GA",
+                "CITY": "Atlanta",
+                "CONTACT NAME": "Emily Strongwater",
+                "OFFICE": "Strongwater & Associates",
+                "PHONE": "404-872-1700"
+            },
+            "createdTime": "2020-09-19T04:55:03.000Z"
+        },
+        {
+            "id": "recddtkAjjTKcGf2x",
+            "fields": {
+                "STATE": "TX",
+                "CITY": "Houston",
+                "CONTACT NAME": "J. Edward Shipp",
+                "PHONE": "404-507-6773"
+            },
+            "createdTime": "2020-09-19T04:55:03.000Z"
+        },
+        {
+            "id": "recoyU4PrrUULDDMp",
+            "fields": {
+                "STATE": "CA",
+                "CITY": "Cupertino",
+                "CONTACT NAME": "Adam Brown",
+                "OFFICE": "Law Office of Adam D. Brown",
+                "PHONE": "404-476-6695"
+            },
+            "createdTime": "2020-09-19T04:55:03.000Z"
+        }
+    ],
+    "offset": "recoyU4PrrUULDDMp"
+};
+
+const flavors = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+];
+const places = [
+    { value: 'CA', label: 'California' },
+    { value: 'TX', label: 'Texas' },
+    { value: 'AR', label: 'Arkansas' },
+];
+const locations = getLocations(sheetInfo);
 
 function QuizTesting() {
-    const flavors = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' },
-    ];
-    const places = [
-        { value: 'CA', label: 'California' },
-        { value: 'TX', label: 'Texas' },
-        { value: 'AR', label: 'Arkansas' },
-    ]
     const placesElement = <SelectElement
         options={places}
         searchable={true}
@@ -49,10 +102,16 @@ function QuizTesting() {
         searchable={false}
         multi={true}
     />
+    const locationsElement = <SelectElement
+        options={locations}
+        searchable={true}
+        multi={false}
+    />
     return (
         <div className="QuizTesting">
             {placesElement}
             {flavorsElement}
+            {locationsElement}
         </div>
     );
 }
