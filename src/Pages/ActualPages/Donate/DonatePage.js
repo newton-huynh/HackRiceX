3e01d7573b699a82dd29237e20e3205c6c337301
Vivian Ellis <vce1@rice.edu>
2020-09-20 04:47:31 -0500

    Correct protesters typo.

--- src/Pages/ActualPages/Donate/DonatePage.js
+++ src/Pages/ActualPages/Donate/DonatePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from "styled-components";
-import {Link, Button} from 'rebass';
+import { Link, Button } from 'rebass';
 
 const DonateWrapper = styled.div`
     display: grid;
@@ -53,26 +53,26 @@ const RightColDonate = styled.div`
 
 function DonatePage() {
     return (
-        <DonateWrapper>  
+        <DonateWrapper>
             <DonateTitle>donate</DonateTitle>
             <DescriptionDonation>
-                Interested in helping pro bono lawyers be compensated for their work? Below are links to two pages: on the left, a place to donate 
-                where your money will be split between 70+ community bail funds, mutual aid funds, and racial justice organizers and on the right, 
+                Interested in helping pro bono lawyers be compensated for their work? Below are links to two pages: on the left, a place to donate
+                where your money will be split between 70+ community bail funds, mutual aid funds, and racial justice organizers and on the right,
                 a GoFundMe for pro bono representation of Houston protesters.
             </DescriptionDonation>
             <LeftColDonate>
                 <p>donate to ActBlue:</p>
                 <form action="https://secure.actblue.com/donate/bail_funds_george_floyd" method="get" target="_blank">
-                    <button type="submit" style={{fontSize:"18px", borderStyle:"solid", borderColor:"#3d5a80", borderWidth:"4px", borderRadius:"10px"}}>ActBlue</button>
+                    <button type="submit" style={{ fontSize: "18px", borderStyle: "solid", borderColor: "#3d5a80", borderWidth: "4px", borderRadius: "10px" }}>ActBlue</button>
                 </form>
             </LeftColDonate>
             <RightColDonate>
-                <p>donate to help Houston protestors get representation:</p>
+                <p>donate to help Houston protesters get representation:</p>
                 <form action="https://www.gofundme.com/f/pro-bono-representation-of-houston-protestors?utm_source=customer&utm_medium=copy_link&utm_campaign=p_cf+share-flow-1" method="get" target="_blank">
-                    <button type="submit" style={{fontSize:"18px", borderStyle:"solid", borderColor:"#3d5a80", borderWidth:"4px", borderRadius:"10px"}}>goFundme</button>
+                    <button type="submit" style={{ fontSize: "18px", borderStyle: "solid", borderColor: "#3d5a80", borderWidth: "4px", borderRadius: "10px" }}>goFundme</button>
                 </form>
             </RightColDonate>
-        </DonateWrapper>    
+        </DonateWrapper>
     )
 }
 
