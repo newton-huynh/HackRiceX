875c81328755ff144acba775456c4eeba4d02206
Anna Bai <alb21@rice.edu>
2020-09-20 01:06:41 -0500

    new how matching works tab and other fixes

--- src/Routing.js
+++ src/Routing.js
@@ -2,8 +2,9 @@ import React from "react";
 import GeneralPageContainer from "./Pages/General/GeneralPageContainer.js";
 import { Route, Switch, Redirect, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./Pages/ActualPages/HomePage.js";
-import AboutUsPage from "./Pages/ActualPages/AboutUsPage.js";
-import Donate from "./Pages/ActualPages/DonatePage.js";
+import AboutUsPage from "./Pages/ActualPages/AboutUs/AboutUsPage.js";
+import HowMatchingWorksPage from "./Pages/ActualPages/HowMatchingWorksPage.js";
+import Donate from "./Pages/ActualPages/Donate/DonatePage.js";
 import TakeQuizPage from "./Pages/ActualPages/TakeQuizPage.js";
 import styled from "styled-components";
 
@@ -14,6 +15,7 @@ function PageRouting() {
         <Redirect to="/home" />
       </Route>
       <Route path="/Home" exact component={HomePage} />
+      <Route path="/HowMatchingWorks" exact component={HowMatchingWorksPage} />
       <Route path="/AboutUs" component={AboutUsPage} />
       <Route path="/Donate" component={Donate} />
       <Route path="/TakeQuiz" component={TakeQuizPage} />
